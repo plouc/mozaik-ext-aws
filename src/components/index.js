a0e75e2c20887ca6d1546890dbc7c4b19f2e30b7
Raphaël Benitte <benitteraphael@gmail.com>
2015-02-20 08:50:06 +0100

    Remove auto component registration

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,4 @@
-var components = {
+module.exports = {
     Instances: require('./Instances.jsx'),
     Stacks:    require('./Stacks.jsx')
 };
-
-require('mozaik/browser')
-    .add('aws.instances', components.Instances)
-    .add('aws.stacks',    components.Stacks)
-;
-
-module.exports = components;

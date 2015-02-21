00449972ef60c5e14d912f51b7106b5bc0be183f
Raphaël Benitte <benitteraphael@gmail.com>
2015-02-21 22:03:16 +0100

    Remove debug code

--- src/preprocessor.js
+++ src/preprocessor.js
@@ -2,11 +2,6 @@ var ReactTools = require('react-tools');
 
 module.exports = {
     process: function (src) {
-
-        //console.log(src);
-
-        //return src;
-
         return ReactTools.transform(src, {
             harmony: true
         });
